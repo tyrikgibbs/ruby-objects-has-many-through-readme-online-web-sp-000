a84cada61cfd0dc947e20d93ba9adf8c3665735e
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-26 04:15:39 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -13,7 +13,11 @@ class Waiter
   def self.all
     @@all
   end
-
+  
+  def new_meal(customer, total, tip=0)
+      Meal.new(self, customer, total, tip)
+    end
+    
   def meals
   Meal.all.select do |meal|
     meal.waiter == self #checking for waiter now
